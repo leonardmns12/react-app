3f0929f9ecf83f8d80b9d7f7612a1b9e3719690f
leonardmns12 <54368620+leonardmns12@users.noreply.github.com>
2020-02-13 13:10:24 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+
+class Timer extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      second : props.second,
+      minute : props.minute,
+      hour   : props.hour
+    }
+    
+
+  }
+
+  componentDidMount(){
+   this.addInterval =  setInterval( () => this.increase() , 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.addInterval)
+  }
+
+  setZero = (state,props) => {
+    let temp = this.state.second;
+    console.log('masuk');
+    if(parseInt(this.state.second) > 5){
+      this.setState({second:0})
+    }
+  }
+ 
+  
+  increase(){
+    this.setState((state,props) => ({
+     second: parseInt(state.second) +1,
+     hour : parseInt(state.hour) +1,
+     minute : parseInt(state.minute) +1
+    }));
+  }
+
+
+  render(){
+    return(
+      <div> {this.state.second} : {this.state.minute} : {this.state.hour} 
+      <br></br>
+      <button onClick={this.setZero}>Klik me</button>
+      </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Timer second="6" minute="0" hour="0" />
       </header>
     </div>
   );
